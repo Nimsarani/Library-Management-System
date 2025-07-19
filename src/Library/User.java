1ec18b1ceee9dc7088ca0a946719fe6617852472
malsha <nimsarani@gmail.com>
2025-07-19 19:22:07 +0530

    initial commit

--- src/Library/User.java
+++ src/Library/User.java
@@ -1,4 +1,24 @@
 package Library;
 
 public class User {
+
+    protected String name;
+    protected String email;
+    protected String phonenumber;
+
+    public User(){
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public String getPhonenumber(){
+        return phonenumber;
+    }
+
 }
